06d37f7df7f2bbfa97a774592c0d932db184c8d3
globalqss <devnull@localhost>
2010-04-24 06:04:57 +0000

    merge -c 12033 -r 12085:12087 -r 12095:12126 -r 12129:12138 from trunk 12033, 12087 -> trifonnt -> FR [2987625] - Add more details to Organization 12086 -> red1 -> simplify params type 12096 -> trifonnt -> Small typo fix. 12097, 12112, 12137 -> trifonnt, globalqss -> FR [2990358] - Extend Initial Tenant setup 12108 -> trifonnt (waqarazeem, needle58) -> BF [2891211] - VDate: once a date is set, its not possible to NULL it 12115 -> genied -> Fixes for AmtInWords_IT.java 12116, 12138 -> freepath, globalqss -> BF [ 2891218] Wrong behavior in entity type settings for customization entity types 12120 -> trifonnt -> BF [2990945] - Show tab images 12126 -> trifonnt -> Added Hormiga Dias to Committers list. 12130 -> trifonnt (petruc) -> BF [2983810] - Zoom across not working properly 12133, 12135 -> vpj-cd -> Error Qty In transit have default 1 and should be null 12136 -> globalqss (jmpiloq) -> BF [2989353] Slow scrolling in Gridview / strange DB access pattern

--- base/src/org/compiere/model/MOrgInfo.java
+++ base/src/org/compiere/model/MOrgInfo.java
@@ -35,23 +35,35 @@ public class MOrgInfo extends X_AD_OrgInfo
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 2079223595471129816L;
-
+	private static final long serialVersionUID = 2496591466841600079L;
 
 	/**
 	 * 	Load Constructor
 	 *	@param ctx context
 	 *	@param AD_Org_ID id
 	 *	@return Org Info
+	 *  @deprecated
 	 */
 	public static MOrgInfo get (Properties ctx, int AD_Org_ID)
+	{
+		return get(ctx, AD_Org_ID, null);
+	}	//	get
+
+	/**
+	 * 	Load Constructor
+	 *	@param ctx context
+	 *	@param AD_Org_ID id
+	 *  @param trxName
+	 *	@return Org Info
+	 */
+	public static MOrgInfo get (Properties ctx, int AD_Org_ID, String trxName)
 	{
 		MOrgInfo retValue = s_cache.get(AD_Org_ID);
 		if (retValue != null)
 		{
 			return retValue;
 		}
-		retValue = new Query(ctx, Table_Name, "AD_Org_ID=?", null)
+		retValue = new Query(ctx, Table_Name, "AD_Org_ID=?", trxName)
 						.setParameters(AD_Org_ID)
 						.firstOnly();
 		if (retValue != null)
@@ -62,7 +74,7 @@ public class MOrgInfo extends X_AD_OrgInfo
 	}	//	get
 
 	/**	Cache						*/
-	private static CCache<Integer,MOrgInfo>	s_cache	= new CCache<Integer,MOrgInfo>(Table_Name, 50);
+	private static CCache<Integer,MOrgInfo>	s_cache	= new CCache<Integer, MOrgInfo>(Table_Name, 50);
 
 	
 	/**************************************************************************
@@ -88,4 +100,4 @@ public class MOrgInfo extends X_AD_OrgInfo
 		setTaxID ("?");
 	}	//	MOrgInfo
 	
-}	//	MOrgInfo
+}
