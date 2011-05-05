ea4c4fe87863c67e2ea1ad5774b8fa35c381bea5
globalqss <devnull@localhost>
2011-05-04 22:51:28 -0500

    Prepare release of branches globalqss adempiere361

--- base/src/org/compiere/Adempiere.java
+++ base/src/org/compiere/Adempiere.java
@@ -60,11 +60,11 @@ public final class Adempiere
 	/** Main Version String         */
 	// Conventions for naming second number is even for stable, and odd for unstable
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
-	static public String	MAIN_VERSION	= "360LTS.010";
+	static public String	MAIN_VERSION	= "360LTS.011";
 	/** Detail Version as date      Used for Client/Server		*/
 	static public String	DATE_VERSION	= "2010-06-14";
 	/** Database Version as date    Compared with AD_System		*/
-	static public String	DB_VERSION		= "2011-02-22";
+	static public String	DB_VERSION		= "2011-05-04";
 
 	/** Product Name            */
 	static public final String	NAME 			= "ADempiere\u00AE";
