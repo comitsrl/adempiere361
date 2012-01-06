f7d017f0f67b2e764985500c29d824d936ea0413
globalqss <devnull@localhost>
2012-01-06 18:30:00 -0500

    Avoid NPE ===========> zkoss.handleError:1130: >>java.lang.NullPointerException >>	at org.adempiere.webui.component.Tab.onClose(Tab.java:46)

--- zkwebui/WEB-INF/src/org/adempiere/webui/component/Tab.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/component/Tab.java
@@ -43,7 +43,8 @@ public class Tab extends org.zkoss.zul.Tab
 	@Override
 	public void onClose() {
 		Tabpanel tp = (Tabpanel) getLinkedPanel();
-		tp.onClose();
+		if (tp != null)
+			tp.onClose();
 	}
 
 }
