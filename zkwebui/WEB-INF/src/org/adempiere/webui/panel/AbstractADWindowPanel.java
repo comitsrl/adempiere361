589d9b1c3be16a4761eea72e3d2f407637ea4b78
hengsin <devnull@localhost>
2012-03-21 22:52:23 -0500

    IDEMPIERE-202 Zk: Zoom to detail tab doesn't trigger display logic

--- zkwebui/WEB-INF/src/org/adempiere/webui/panel/AbstractADWindowPanel.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/panel/AbstractADWindowPanel.java
@@ -498,7 +498,7 @@ public abstract class AbstractADWindowPanel extends AbstractUIPart implements To
     							if (pindex >= 0)
     								setActiveTab(pindex);
     						}
-    						gTab.setCurrentRow(i);
+    						gTab.navigate(i);
     						return true;
     					}
     				}
