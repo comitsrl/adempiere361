f80598a761637ba37086c81c5ffd41ac97d2e43a
globalqss <devnull@localhost>
2012-09-12 10:13:26 -0500

    IDEMPIERE-417 Update BPLocation.Name

--- base/src/org/compiere/model/MLocation.java
+++ base/src/org/compiere/model/MLocation.java
@@ -656,12 +656,14 @@ public class MLocation extends X_C_Location implements Comparator
 				+ " OR C_LocTo_ID=" + getC_Location_ID() + ")", get_TrxName());
 		
 		//Update BP_Location name IDEMPIERE 417
-		int bplID = DB.getSQLValueEx(get_TrxName(), "SELECT C_BPartner_Location_ID FROM C_BPartner_Location WHERE C_Location_ID = " + getC_Location_ID());
-		if (bplID>0)
-		{
-			MBPartnerLocation bpl = new MBPartnerLocation(getCtx(), bplID, get_TrxName());
-			bpl.setName(bpl.getBPLocName(this));
-			bpl.saveEx();
+		if (get_TrxName().startsWith("POSave")) { // saved without trx
+			int bplID = DB.getSQLValueEx(get_TrxName(), "SELECT C_BPartner_Location_ID FROM C_BPartner_Location WHERE C_Location_ID = " + getC_Location_ID());
+			if (bplID>0)
+			{
+				MBPartnerLocation bpl = new MBPartnerLocation(getCtx(), bplID, get_TrxName());
+				bpl.setName(bpl.getBPLocName(this));
+				bpl.saveEx();
+			}
 		}
 		return success;
 	}	//	afterSave
