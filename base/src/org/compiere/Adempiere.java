e8f23fb408c3aec310cc42a442732ab74e5735ab
sim4s <santi.imas37@gmail.com>
2024-05-21 13:35:20 -0300

    Se actualiza versión e información de publicación

--- base/src/org/compiere/Adempiere.java
+++ base/src/org/compiere/Adempiere.java
@@ -60,11 +60,11 @@ public final class Adempiere
 	/** Main Version String         */
 	// Conventions for naming second number is even for stable, and odd for unstable
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
-	static public String	MAIN_VERSION	= "361LTS.Final";
+	static public String	MAIN_VERSION	= "362.2";
 	/** Detail Version as date      Used for Client/Server		*/
-	static public String	DATE_VERSION	= "2012-11-15";
+	static public String	DATE_VERSION	= "2024-05-21";
 	/** Database Version as date    Compared with AD_System		*/
-	static public String	DB_VERSION		= "2012-11-15";
+	static public String	DB_VERSION		= "2024-05-21";
 
 	/** Product Name            */
 	static public final String	NAME 			= "ADempiere\u00AE";
